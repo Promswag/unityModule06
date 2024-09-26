59eca9027e749f16000b42a1d541cf00aa7317ae
Guillaume Baumgartner <promswag@gmai.com>
2024-09-26 18:51:08 +0200

    wip

--- Module06/Assets/Scripts/TextureTilingOffset.cs
+++ Module06/Assets/Scripts/TextureTilingOffset.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class TextureTilingOffset : MonoBehaviour
 {
     [SerializeField] private Vector2 tiling = new();
@@ -8,13 +7,6 @@ public class TextureTilingOffset : MonoBehaviour
     [SerializeField] private MeshRenderer meshRenderer;
 
     void Start()
-    {
-        // meshRenderer = GetComponent<MeshRenderer>();
-        // tiling = new(1, 1);
-        // offset = new(0, 0);
-    }
-
-    void Update()
     {
         meshRenderer.material.SetTextureScale("_MainTex", tiling);
         meshRenderer.material.SetTextureOffset("_MainTex", offset);
